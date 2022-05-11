2686d88a9508bee70e5c59152131c2eaf48110df
thearkamitra <thearkamitra@gmail.com>
2022-05-12 00:05:24 +0200

    Error with output dimensions

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from albumentations.pytorch import ToTensorV2
 from torchvision.models.segmentation import fcn_resnet50
 import pdb
 from utils import train, test
-from torch.optim import Adam
-from torch.optim import lr_scheduler
+from torch.optim import Adam,lr_scheduler
+from torch.utils.data import random_split, DataLoader
 train_transform = Alb.Compose(
         [
             Alb.RandomRotate90(p=0.6),
@@ -35,13 +35,21 @@ test_transform = Alb.Compose(
     )
 
 
-train_dataset = RoadCIL("training", training=True, transform=train_transform)
+dataset = RoadCIL("training", training=True, transform=train_transform)
 test_dataset = RoadCIL("test", training=False, transform=test_transform)
+
+validation_length = int(2*len(dataset)//10)
+train_dataset, validation_dataset = random_split(dataset, [(len(dataset) - validation_length), validation_length])
+
+batch_size = 4
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 model = fcn_resnet50(pretrained=True)
 loss = nn.BCELoss()
 optimizer = Adam(model.parameters(), 1e-4)
 scheduler = lr_scheduler.ReduceLROnPlateau(optimizer)
-train(model, train_dataset, loss, optimizer, scheduler)
+train(model, train_dataloader, validation_dataloader,  loss, optimizer, scheduler)
 
 test(model, test_dataset,  loss, )
--- utils.py
+++ utils.py
@@ -1,9 +1,55 @@
 import torch
 import torch.nn as nn
+import pdb
+
+def train(model, train_dataset, val_dataset, loss, optimizer, scheduler, \
+            epochs=4, model_name = "First_check.pth"):
+    loss_val_min = torch.tensor(1e10)
+    loss_min = torch.tensor(1e10) #Min loss for comparison and saving best models
+    for epoch in range(epochs):    
+        print(f"Epoch {epoch} training started.")
+        train_epoch(model, train_dataset, optimizer, loss)
+        print(f"Epoch {epoch} validation started.")
+        loss_val = val_epoch(model, val_dataset, epoch, loss)
+        scheduler.step(loss_val) #Scheduler changes learning rate based on criterion
+        if loss_val<loss_min: #Model saved if min val loss obtained
+            print("Model weights are saved.")
+            loss_min = loss_val
+            torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'loss': loss_val,
+            }, model_name)
 
 
-def train(model, train_dataset, loss, optimizer, scheduler):
-    pass
+def train_epoch(model, train_dataset, optimizer, loss_func):
+    '''
+    Training per epoch
+    '''
+    for idx, batch in enumerate(train_dataset):
+        # pdb.set_trace()
+        optimizer.zero_grad()
+        images, masks = batch
+        out = model(images)
+        out = out['out']
+        pdb.set_trace()
+        loss = loss_func(out, masks.unsqueeze(1))
+        loss.backward()
+        optimizer.step() #Weights are updated
+
+
+
+def val_epoch(model, val_dataset, loss_func):
+    '''
+    Validation Step after each epoch
+    '''
+    loss_val = 0
+    for idx, batch in enumerate(val_dataset):
+        images, masks = batch
+        out = model(images)
+        loss = loss_func(out, masks)
+        loss_val+= loss 
+    return loss_val
 
 def test(model, test_dataset,  loss, ):
     pass
