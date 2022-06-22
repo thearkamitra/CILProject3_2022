fd1eef8984161c92013e7adc4b56779072d6e2f3
Ambi <ambarish004@gmail.com>
2022-06-22 14:59:34 +0200

    Oops, smaller changes to run script

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-bsub -n 2 -W 300 -R "rusage[mem=6000,ngpus_excl_p=1]"  "python main.py --batch 8 --model unet --wandb True --modelname unet_diceloss.pth"
+bsub -n 2 -W 300 -R "rusage[mem=6000,ngpus_excl_p=1]"  "python main.py --batch 8 --model unet --wandb"
